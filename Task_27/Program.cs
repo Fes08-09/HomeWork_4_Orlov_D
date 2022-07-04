1fd9851e73a4d1be452f0827ff07c68b0bd31d2c
Dmitriy <Fes08-09@yandex.ru>
2022-07-04 23:48:43 +0300

    Task_27_input number -> count sum of charactersin number

---
+++ Task_27/Program.cs
@@ -0,0 +1,30 @@
+﻿// // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число:");
+int n = int.Parse(ReadLine());
+int array = sum(n);
+WriteLine($"{n} -> {array}");
+
+int sum (int r)
+{
+    int result = 0;
+    string str = r.ToString();
+    int[] sum = new int[str.Length];
+    int i = 0;
+    for(i=0; i< str.Length; i++)
+    {
+        sum[i] = int.Parse(str[i].ToString());
+        result+=sum[i]; 
+    }
+    return result;
+}
+
+
