21a8be99987003c1a75520ca1dc46526c3d20d8f
Dmitriy <Fes08-09@yandex.ru>
2022-07-04 23:47:35 +0300

    Task_25_A in grede B

---
+++ Task_25/Program.cs
@@ -0,0 +1,21 @@
+﻿// // Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число А: ");
+int A = int.Parse(ReadLine());
+WriteLine("Введите число B: ");
+int B = int.Parse(ReadLine());
+double res = 0;
+WriteLine($"{A}, {B} -> {grade(res)}");
+
+double grade(double r)
+{
+    double result = Math.Pow(A, B);
+    return result;
+}
